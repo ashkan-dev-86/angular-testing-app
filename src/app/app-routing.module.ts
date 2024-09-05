392bda784ea8581083a57695c9a2cb45a797f216
ashkan-dev-86 <ashi.dev.86@gmail.com>
2024-09-05 22:53:15 +0800

    Add html tags

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: 'accordion',
-        component: LayoutComponent
+        component: AccordionComponent
       }
     ]
   }
---
+++ src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss'],
+})
+export class AccordionComponent {
+  public progress = 0;
+
+  private step = 100 / 15;
+
+  constructor(
+    private changeDetector: ChangeDetectorRef
+  ) {
+    this.progressTheSeconds();
+  }
+
+  private progressTheSeconds(): void {
+    setInterval(() => {
+      if (this.progress < 100) {
+        this.progress += this.step;
+        this.changeDetector.detectChanges();
+      }
+    }, 1000);
+  }
+}
